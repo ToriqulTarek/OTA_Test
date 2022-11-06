96507497509717fc565a35657f52530fa3173459
Toriqul_Islam <33224137+ToriqulTarek@users.noreply.github.com>
2022-11-06 20:02:00 +0600

    Add files via upload

---
+++ HTTPS_OTA_Update.ino
@@ -0,0 +1,94 @@
+// This sketch provide the functionality of OTA Firmware Upgrade
+#include "WiFi.h"
+#include "HttpsOTAUpdate.h"
+// This sketch shows how to implement HTTPS firmware update Over The Air.
+// Please provide your WiFi credentials, https URL to the firmware image and the server certificate.
+
+static const char *ssid     = "ANTT Robotics TP";  // your network SSID (name of wifi network)
+static const char *password = "digi@anti@123"; // your network password
+
+static const char *url = "https://raw.githubusercontent.com/ToriqulTarek/OTA_Test/main/Blink.ino.esp32.bin"; //state url of your firmware image
+
+static const char *server_certificate =
+  "-----BEGIN CERTIFICATE-----\n" \
+  "MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n" \
+  "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
+  "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n" \
+  "QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT\n" \
+  "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n" \
+  "b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG\n" \
+  "9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB\n" \
+  "CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97\n" \
+  "nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt\n" \
+  "43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P\n" \
+  "T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4\n" \
+  "gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO\n" \
+  "BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR\n" \
+  "TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw\n" \
+  "DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr\n" \
+  "hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg\n" \
+  "06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF\n" \
+  "PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n" \
+  "YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n" \
+  "CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=\n" \
+  "-----END CERTIFICATE-----";
+
+static HttpsOTAStatus_t otastatus;
+
+void HttpEvent(HttpEvent_t *event)
+{
+  switch (event->event_id) {
+    case HTTP_EVENT_ERROR:
+      Serial.println("Http Event Error");
+      break;
+    case HTTP_EVENT_ON_CONNECTED:
+      Serial.println("Http Event On Connected");
+      break;
+    case HTTP_EVENT_HEADER_SENT:
+      Serial.println("Http Event Header Sent");
+      break;
+    case HTTP_EVENT_ON_HEADER:
+      Serial.printf("Http Event On Header, key=%s, value=%s\n", event->header_key, event->header_value);
+      break;
+    case HTTP_EVENT_ON_DATA:
+      break;
+    case HTTP_EVENT_ON_FINISH:
+      Serial.println("Http Event On Finish");
+      break;
+    case HTTP_EVENT_DISCONNECTED:
+      Serial.println("Http Event Disconnected");
+      break;
+  }
+}
+
+void setup() {
+
+  Serial.begin(115200);
+  Serial.print("Attempting to connect to SSID: ");
+  WiFi.begin(ssid, password);
+
+  // attempt to connect to Wifi network:
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+
+  HttpsOTA.onHttpEvent(HttpEvent);
+  Serial.println("Starting OTA");
+  HttpsOTA.begin(url, server_certificate);
+
+  Serial.println("Please Wait it takes some time ...");
+}
+
+void loop() {
+  otastatus = HttpsOTA.status();
+  if (otastatus == HTTPS_OTA_SUCCESS) {
+    Serial.println("Firmware written successfully. To reboot device, call API ESP.restart() or PUSH restart button on device");
+  } else if (otastatus == HTTPS_OTA_FAIL) {
+    Serial.println("Firmware Upgrade Fail");
+  }
+  delay(1000);
+}
